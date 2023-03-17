be918dd3b3f93850af903ffc582a35e115219976
Aleksandr Eskov <mullikaloll@yandex.ru>
2023-03-17 15:57:22 +0500

    db

--- bot.py
+++ bot.py
@@ -27,11 +27,11 @@ async def main():
 
     postgres_url = URL.create(
         'postgresql+asyncpg',
-        username='kotofey',
+        username='postgres',
         password='lol4ik594770146',
         host='localhost',
         port=5432,
-        database='sammy'
+        database='postgres'
 
     )
 
